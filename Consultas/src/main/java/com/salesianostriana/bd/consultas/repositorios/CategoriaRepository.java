db2935b535b272240cc67aab2c90b4e2c16169e7
Lusimi <luismi.lopez@salesianos.edu>
2018-05-17 14:08:35 +0200

    Cambios menores

--- Consultas/src/main/java/com/salesianostriana/bd/consultas/repositorios/CategoriaRepository.java
+++ Consultas/src/main/java/com/salesianostriana/bd/consultas/repositorios/CategoriaRepository.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.salesianostriana.bd.consultas.modelo.Categoria;
 
@@ -14,8 +15,18 @@ public interface CategoriaRepository extends JpaRepository<Categoria, Long> {
 	@Query("select c.nombre from Categoria c")
 	List<String> findAllCategories();
 	
+	// Obtener todas las categorias que contengan una parte del nombre
+	@Query("select c from Categoria c where c.nombre LIKE ?1")
+	List<Categoria> encuentraCategoriaPorNombre(String nombre);
+
+	@Query("select c from Categoria c where c.nombre LIKE :nombre")
+	List<Categoria> encuentraCategoriaPorNombrev2(@Param("nombre") String nombre);
+
+	
 	// Obtener las 5 primeras categorías ordenadas alfabéticamente por el nombre
 	List<Categoria> findFirst5ByOrderByNombreAsc();
+	
+	List<Categoria> findByOrderByNombreAsc();
 	
 	// Obtener una categoría cuyo nombre contenga una cadena de caracteres
 	Categoria findFirstByNombreContainingIgnoreCase(String nombre);
--- SeguridadElemental03_Herencia/src/main/java/com/salesianostriana/bd/seguridadelemental/service/UsuarioService.java
+++ SeguridadElemental03_Herencia/src/main/java/com/salesianostriana/bd/seguridadelemental/service/UsuarioService.java
@@ -10,30 +10,28 @@ import com.salesianostriana.bd.seguridadelemental.model.Usuario;
 
 @Service
 public class UsuarioService {
-	
-	/*@Autowired
-	private UsuarioRepository repositorio;*/
-	
+
+	/*
+	 * @Autowired private UsuarioRepository repositorio;
+	 */
+
 	@PersistenceContext
 	EntityManager entityManager;
-	
-	
+
 	public Usuario login(String username, String pass) {
-		//return repositorio.findFirstByUsernameAndPass(username, pass);
-		
-		//Query q = entityManager.createQuery("");
-		TypedQuery<Usuario> query = (TypedQuery<Usuario>) entityManager.createQuery("select distinct u from Usuario u where u.username = ?1 and u.pass = ?2");
+		// return repositorio.findFirstByUsernameAndPass(username, pass);
+
+		// Query q = entityManager.createQuery("");
+		TypedQuery<Usuario> query = (TypedQuery<Usuario>) entityManager
+				.createQuery("select distinct u from Usuario u where u.username = ?1 and u.pass = ?2");
 		query.setParameter(1, username);
 		query.setParameter(2, pass);
 		query.setFirstResult(0);
 		query.setMaxResults(1);
-		
+
 		Usuario result = query.getSingleResult();
-		
+
 		return result;
 	}
-	
-	
-	
 
 }
